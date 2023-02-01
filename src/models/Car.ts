50316d8bdced805e401259d8eaf9c3be5736a32e
alinekabbas <alinekabbas@gmail.com>
2023-02-01 17:36:23 -0300

    exercicio concluido

---
+++ src/models/Car.ts
@@ -0,0 +1,50 @@
+export class Car {
+    constructor(
+        private id: string,
+        private make: string,
+        private model: string,
+        private color: string,
+        private year: number
+    ) { }
+
+    public getId(): string {
+        return this.id
+    }
+
+    public setId(value: string): void {
+        this.id = value
+    }
+
+    public getMake(): string {
+        return this.make
+    }
+
+    public setMake(value: string): void {
+        this.make = value
+    }
+
+    public getModel(): string {
+        return this.model
+    }
+
+    public setModel(value: string): void {
+        this.model = value
+    }
+
+    public getColor(): string {
+        return this.color
+    }
+
+    public setColor(value: string): void {
+        this.color = value
+    }
+
+    public getYear(): number {
+        return this.year
+    }
+
+    public setYear(value: number): void {
+        this.year = value
+    }
+}
+
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export interface CarDB {
+    id: string,
+    make: string,
+    model: string,
+    color: string,
+    year: number
+}
